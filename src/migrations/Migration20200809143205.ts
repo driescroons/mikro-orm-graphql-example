b5a39c91ba5668b33cea44f7b3a5c068b8371cb1
Olivier Refalo <orefalo@snapcore.com>
2022-11-30 15:29:21 +0400

    ch

--- src/migrations/Migration20200809143205.ts
+++ src/migrations/Migration20200809143205.ts
@@ -3,27 +3,27 @@ import { Migration } from '@mikro-orm/migrations'
 export class Migration20200809143205 extends Migration {
     async up(): Promise<void> {
         this.addSql(
-            'create table "tag" ("id" uuid not null, "created_at" timestamptz(0) not null, "updated_at" timestamptz(0) not null, "name" varchar(255) not null);'
+            'create table "tag" ("id" varchar(21) not null, "created_at" timestamptz(0) not null, "updated_at" timestamptz(0) not null, "name" varchar(255) not null);'
         )
         this.addSql('alter table "tag" add constraint "tag_pkey" primary key ("id");')
 
         this.addSql(
-            'create table "publisher" ("id" uuid not null, "created_at" timestamptz(0) not null, "updated_at" timestamptz(0) not null, "name" varchar(255) not null, "type" text check ("type" in (\'local\', \'global\')) not null);'
+            'create table "publisher" ("id" varchar(21) not null, "created_at" timestamptz(0) not null, "updated_at" timestamptz(0) not null, "name" varchar(255) not null, "type" text check ("type" in (\'local\', \'global\')) not null);'
         )
         this.addSql('alter table "publisher" add constraint "publisher_pkey" primary key ("id");')
 
         this.addSql(
-            'create table "author" ("id" uuid not null, "created_at" timestamptz(0) not null, "updated_at" timestamptz(0) not null, "name" varchar(255) not null, "email" varchar(255) not null, "born" timestamptz(0) null, "favourite_book_id" uuid null);'
+            'create table "author" ("id" varchar(21) not null, "created_at" timestamptz(0) not null, "updated_at" timestamptz(0) not null, "name" varchar(255) not null, "email" varchar(255) not null, "born" timestamptz(0) null, "favourite_book_id" varchar(21) null);'
         )
         this.addSql('alter table "author" add constraint "author_pkey" primary key ("id");')
         this.addSql('alter table "author" add constraint "author_email_unique" unique ("email");')
 
         this.addSql(
-            'create table "book" ("id" uuid not null, "created_at" timestamptz(0) not null, "updated_at" timestamptz(0) not null, "title" varchar(255) not null, "author_id" uuid not null, "publisher_id" uuid null);'
+            'create table "book" ("id" varchar(21) not null, "created_at" timestamptz(0) not null, "updated_at" timestamptz(0) not null, "title" varchar(255) not null, "author_id" varchar(21) not null, "publisher_id" varchar(21) null);'
         )
         this.addSql('alter table "book" add constraint "book_pkey" primary key ("id");')
 
-        this.addSql('create table "book_tags" ("book_id" uuid not null, "tag_id" uuid not null);')
+        this.addSql('create table "book_tags" ("book_id" varchar(21) not null, "tag_id" varchar(21) not null);')
         this.addSql(
             'alter table "book_tags" add constraint "book_tags_pkey" primary key ("book_id", "tag_id");'
         )
